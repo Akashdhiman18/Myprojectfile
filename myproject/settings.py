656ee2557c991358b32d5f9676088e76f30e9205
Akash Dhiman <akashdhiman181999@gmail.com>
2024-03-08 13:26:20 +1300

    Trying with the if part of the db

--- myproject/settings.py
+++ myproject/settings.py
@@ -94,8 +94,18 @@ DATABASES = {
 }
 '''
 
+DATABASES = {
+        'default': {
+            'ENGINE':'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -106,6 +116,7 @@ DATABASES = {
         'PORT':'5432',
     },
 }
+'''
 
 '''
 if 'RDS_DB_NAME' in os.environ:
